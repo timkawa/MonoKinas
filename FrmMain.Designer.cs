d5a9024428deaeba7b05d5ced82d1e1d817e4ec6
timkawa <54140050+timkawa@users.noreply.github.com>
2022-08-04 11:22:53 +0100

    Base
    Primeira modificação

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Siap
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.administrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.classesMilitaresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.patentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.funçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripSeparator();
+            this.habilitaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dadosAgentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lançamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controloDeEfectividadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripSeparator();
+            this.controloDeAusençiasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosDoSistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripSeparator();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.administrarToolStripMenuItem,
+            this.agentesToolStripMenuItem,
+            this.lançamentosToolStripMenuItem,
+            this.sistemaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 4);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(694, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(887, 490);
+            this.panel1.TabIndex = 1;
+            // 
+            // administrarToolStripMenuItem
+            // 
+            this.administrarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.unidadeToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.classesMilitaresToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.patentesToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.funçõesToolStripMenuItem,
+            this.toolStripMenuItem5,
+            this.habilitaçõesToolStripMenuItem});
+            this.administrarToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.administrarToolStripMenuItem.Name = "administrarToolStripMenuItem";
+            this.administrarToolStripMenuItem.Size = new System.Drawing.Size(81, 26);
+            this.administrarToolStripMenuItem.Text = "Administrar";
+            this.administrarToolStripMenuItem.DropDownClosed += new System.EventHandler(this.administrarToolStripMenuItem_DropDownClosed);
+            this.administrarToolStripMenuItem.DropDownOpened += new System.EventHandler(this.administrarToolStripMenuItem_DropDownOpened);
+            // 
+            // unidadeToolStripMenuItem
+            // 
+            this.unidadeToolStripMenuItem.Name = "unidadeToolStripMenuItem";
+            this.unidadeToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.unidadeToolStripMenuItem.Text = "Unidade";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(157, 6);
+            // 
+            // classesMilitaresToolStripMenuItem
+            // 
+            this.classesMilitaresToolStripMenuItem.Name = "classesMilitaresToolStripMenuItem";
+            this.classesMilitaresToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.classesMilitaresToolStripMenuItem.Text = "Classes Militares";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(157, 6);
+            // 
+            // patentesToolStripMenuItem
+            // 
+            this.patentesToolStripMenuItem.Name = "patentesToolStripMenuItem";
+            this.patentesToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.patentesToolStripMenuItem.Text = "Patentes";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(157, 6);
+            // 
+            // funçõesToolStripMenuItem
+            // 
+            this.funçõesToolStripMenuItem.Name = "funçõesToolStripMenuItem";
+            this.funçõesToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.funçõesToolStripMenuItem.Text = "Funções";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(157, 6);
+            // 
+            // habilitaçõesToolStripMenuItem
+            // 
+            this.habilitaçõesToolStripMenuItem.Name = "habilitaçõesToolStripMenuItem";
+            this.habilitaçõesToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.habilitaçõesToolStripMenuItem.Text = "Habilitações";
+            // 
+            // agentesToolStripMenuItem
+            // 
+            this.agentesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dadosAgentesToolStripMenuItem});
+            this.agentesToolStripMenuItem.Name = "agentesToolStripMenuItem";
+            this.agentesToolStripMenuItem.Size = new System.Drawing.Size(62, 26);
+            this.agentesToolStripMenuItem.Text = "Agentes";
+            // 
+            // dadosAgentesToolStripMenuItem
+            // 
+            this.dadosAgentesToolStripMenuItem.Name = "dadosAgentesToolStripMenuItem";
+            this.dadosAgentesToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.dadosAgentesToolStripMenuItem.Text = "Dados Agentes";
+            // 
+            // lançamentosToolStripMenuItem
+            // 
+            this.lançamentosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.controloDeEfectividadeToolStripMenuItem,
+            this.toolStripMenuItem4,
+            this.controloDeAusençiasToolStripMenuItem});
+            this.lançamentosToolStripMenuItem.Name = "lançamentosToolStripMenuItem";
+            this.lançamentosToolStripMenuItem.Size = new System.Drawing.Size(90, 26);
+            this.lançamentosToolStripMenuItem.Text = "Lançamentos";
+            // 
+            // controloDeEfectividadeToolStripMenuItem
+            // 
+            this.controloDeEfectividadeToolStripMenuItem.Name = "controloDeEfectividadeToolStripMenuItem";
+            this.controloDeEfectividadeToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.controloDeEfectividadeToolStripMenuItem.Text = "Controlo de Efectividade";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(201, 6);
+            // 
+            // controloDeAusençiasToolStripMenuItem
+            // 
+            this.controloDeAusençiasToolStripMenuItem.Name = "controloDeAusençiasToolStripMenuItem";
+            this.controloDeAusençiasToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.controloDeAusençiasToolStripMenuItem.Text = "Controlo de Ausençias";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuariosDoSistemaToolStripMenuItem,
+            this.toolStripMenuItem6,
+            this.sairToolStripMenuItem});
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(60, 26);
+            this.sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // usuariosDoSistemaToolStripMenuItem
+            // 
+            this.usuariosDoSistemaToolStripMenuItem.Name = "usuariosDoSistemaToolStripMenuItem";
+            this.usuariosDoSistemaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usuariosDoSistemaToolStripMenuItem.Text = "Usuarios do Sistema";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(177, 6);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.Black;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.Location = new System.Drawing.Point(850, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(25, 25);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(887, 527);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Central Administrativo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem administrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem classesMilitaresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem patentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem funçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem habilitaçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dadosAgentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lançamentosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controloDeEfectividadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem controloDeAusençiasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosDoSistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Siap
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void administrarToolStripMenuItem_DropDownOpened(object sender, EventArgs e)
+        {
+            administrarToolStripMenuItem.ForeColor = Color.Black;
+        }
+
+        private void administrarToolStripMenuItem_DropDownClosed(object sender, EventArgs e)
+        {
+            administrarToolStripMenuItem.ForeColor = Color.White;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
